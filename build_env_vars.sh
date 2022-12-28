af84d7ab700c0d9d9a96fb3bf569686c69e94279
Lucas Aurelio <lucasmaple625@gmail.com>
2022-12-28 07:40:19 -0300

    preparing env vars

---
+++ build_env_vars.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+##### Development #####
+
+# Frontend
+export ENV_FILE="frontend/.env"
+
+[ -f "$ENV_FILE" ] && rm "$ENV_FILE"
+
+echo 'VUE_APP_MSC_EXPERIMENTER_URL=http://127.0.0.1:8001' >> $ENV_FILE
+echo 'VUE_APP_MSC_ANALYZER_URL=http://127.0.0.1:8000' >> $ENV_FILE
+
+# Analyzer
+export ENV_FILE="analyzer/.env"
+
+[ -f "$ENV_FILE" ] && rm "$ENV_FILE"
+
+echo 'SECRET_KEY="test secret key"' >> $ENV_FILE
+echo 'DEBUG=True' >> $ENV_FILE
+
+##### Kubernetes #####
+
+export ENV_FILE="env_vars.sh"
+
+[ -f "$ENV_FILE" ] && rm "$ENV_FILE"
+
+echo '#!/bin/bash' >> env_vars.sh
+
+# General
+echo '# General' >> $ENV_FILE
+echo 'export MSC_TAG_PREFIX="gcr.io/distributed-ray-tracing"' >> $ENV_FILE
+echo 'export MSC_TAG_PREFIX_FOR_REPLACEMENT="gcr.io\/distributed-ray-tracing"' >> $ENV_FILE
+echo 'export MSC_TAG_VERSION="1.0"' >> $ENV_FILE
+echo 'export MSC_IMAGE_PULL_POLICY="Always"' >> $ENV_FILE
+
+# MSC Analyzer
+echo '# MSC Analyzer' >> $ENV_FILE
+echo 'export MSC_ANALYZER_SECRET_KEY=test-secret-key' >> $ENV_FILE
+
+chmod +x $ENV_FILE

3d24a566d334a39e2964ea0214a666b62d1cdd7e
Lucas Aurelio <lucasmaple625@gmail.com>
2022-12-28 07:41:11 -0300

    Removing env_vars.sh

--- env_vars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# General
-export MSC_TAG_PREFIX="gcr.io/distributed-ray-tracing"
-export MSC_TAG_PREFIX_FOR_REPLACEMENT="gcr.io\/distributed-ray-tracing"
-export MSC_TAG_VERSION="1.0"
-export MSC_IMAGE_PULL_POLICY="Always"
-# MSC Analyzer
-export MSC_ANALYZER_SECRET_KEY=test-secret-key

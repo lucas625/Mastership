608993ccb92198278f13345946b6810e5bfca8b3
Lucas Aurelio <lucasmaple625@gmail.com>
2023-06-09 11:27:49 -0300

    starting linkerd configs

---
+++ scripts/configure_linkerd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+. ./env_vars.sh || exit 1
+
+export MSC_ROOT_FOLDER="${PWD%/*}" 
+export MSC_KUBERNETES_FOLDER="$MSC_ROOT_FOLDER/kubernetes"
+export MSC_LINKERD_FOLDER="$MSC_KUBERNETES_FOLDER/linkerd"
+
+# Allow Build linkerd elements
+cat $MSC_LINKERD_FOLDER/calculator_server.yaml | sed \
+    -e "s/\$\$MSC_NAMESPACE/$MSC_NAMESPACE/" |
+    kubectl apply -n $MSC_NAMESPACE -f -
+cat $MSC_LINKERD_FOLDER/calculator_authorization_policy.yaml | sed \
+    -e "s/\$\$MSC_NAMESPACE/$MSC_NAMESPACE/" |
+    kubectl apply -n $MSC_NAMESPACE -f -
+cat $MSC_LINKERD_FOLDER/mesh_tls_authentication.yaml | sed \
+    -e "s/\$\$MSC_NAMESPACE/$MSC_NAMESPACE/" |
+    kubectl apply -n $MSC_NAMESPACE -f -
+cat $MSC_LINKERD_FOLDER/force_mtls_authorization_policy.yaml | sed \
+    -e "s/\$\$MSC_NAMESPACE/$MSC_NAMESPACE/" |
+    kubectl apply -n $MSC_NAMESPACE -f -
+
+# Run linkerd
+kubectl get -n $MSC_NAMESPACE deploy -o yaml \
+  | linkerd inject - \
+  | kubectl apply -f -

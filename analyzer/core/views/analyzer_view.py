ee0f97f1f65f3bcabe33d732879e606b9638f484
Lucas Aurelio <lucasmaple625@gmail.com>
2023-06-13 17:35:51 -0300

    ADDING REQUEST FROM ANALYZER TO CALCULATOR

--- analyzer/core/views/analyzer_view.py
+++ analyzer/core/views/analyzer_view.py
@@ -1,5 +1,9 @@
+import http
+
+import requests
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_500_INTERNAL_SERVER_ERROR
 from rest_framework.views import APIView
 
 from core.business import AnalyzerBusiness
@@ -8,7 +12,21 @@ from core.serializers import AnalyzerSerializer
 
 class AnalyzerView(APIView):
 
-    http_method_names = ['post']
+    http_method_names = ['get', 'post']
+
+    def get(self, request, *args, **kwargs):
+        """
+        A dummy method for pinging the calculator service when using kubernetes, to see if access to it is enabled or not.
+        """
+        data = {
+            'firstNumber': 10,
+            'secondNumber': 20
+        }
+        try:
+            response = requests.post('http://msc-calculator:8000/add', json=data)
+        except Exception as exc:
+            response = Response(status=HTTP_500_INTERNAL_SERVER_ERROR, data=exc)
+        return response
 
     def post(self, request, *args, **kwargs):
         serializer = AnalyzerSerializer(data=request.data)

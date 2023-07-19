e749f7b6078c93e844b95df5bf3d771fa32d0e5c
Lucas Aurelio <lucasmaple625@gmail.com>
2023-07-19 08:36:02 -0300

    external analyzer

--- scripts/build_external_analyzer.sh
+++ scripts/build_external_analyzer.sh
@@ -5,16 +5,20 @@ export MSC_IMAGE_PULL_POLICY="IfNotPresent"
 export MSC_ROOT_FOLDER="${PWD%/*}" 
 export MSC_KUBERNETES_FOLDER="$MSC_ROOT_FOLDER/kubernetes"
 
-cat $MSC_KUBERNETES_FOLDER/analyzer/analyzer.yaml | sed \
-    -e "s/\$\$MSC_TAG_PREFIX/$MSC_TAG_PREFIX_FOR_REPLACEMENT/" \
-    -e "s/\$\$MSC_TAG_VERSION/$MSC_TAG_VERSION/" \
-    -e "s/\$\$MSC_IMAGE_PULL_POLICY/$MSC_IMAGE_PULL_POLICY/" \
-    -e "s/\$\$MSC_ANALYZER_SECRET_KEY/$MSC_ANALYZER_SECRET_KEY/" | \
-    kubectl delete -n $MSC_NAMESPACE -f -
+export MSC_EXTERNAL_NAMESPACE="default"
 
 cat $MSC_KUBERNETES_FOLDER/analyzer/analyzer.yaml | sed \
+    -e "s/\$\$MSC_NAMESPACE/$MSC_NAMESPACE/" \
     -e "s/\$\$MSC_TAG_PREFIX/$MSC_TAG_PREFIX_FOR_REPLACEMENT/" \
     -e "s/\$\$MSC_TAG_VERSION/$MSC_TAG_VERSION/" \
     -e "s/\$\$MSC_IMAGE_PULL_POLICY/$MSC_IMAGE_PULL_POLICY/" \
     -e "s/\$\$MSC_ANALYZER_SECRET_KEY/$MSC_ANALYZER_SECRET_KEY/" | \
-    kubectl apply -n $MSC_NAMESPACE -f -
+    kubectl delete -n $MSC_EXTERNAL_NAMESPACE -f -
+
+cat $MSC_KUBERNETES_FOLDER/analyzer/analyzer.yaml | sed \
+    -e "s/\$\$MSC_NAMESPACE/$MSC_NAMESPACE/" \
+    -e "s/\$\$MSC_TAG_PREFIX/$MSC_TAG_PREFIX_FOR_REPLACEMENT/" \
+    -e "s/\$\$MSC_TAG_VERSION/$MSC_TAG_VERSION/" \
+    -e "s/\$\$MSC_IMAGE_PULL_POLICY/$MSC_IMAGE_PULL_POLICY/" \
+    -e "s/\$\$MSC_ANALYZER_SECRET_KEY/$MSC_ANALYZER_SECRET_KEY/" | \
+    kubectl apply -n $MSC_EXTERNAL_NAMESPACE -f -
--- scripts/remove_kubernetes_elements.sh
+++ scripts/remove_kubernetes_elements.sh
@@ -6,6 +6,7 @@ export MSC_ROOT_FOLDER="${PWD%/*}"
 export MSC_KUBERNETES_FOLDER="$MSC_ROOT_FOLDER/kubernetes"
 
 cat $MSC_KUBERNETES_FOLDER/analyzer/analyzer.yaml | sed \
+    -e "s/\$\$MSC_NAMESPACE/$MSC_NAMESPACE/" \
     -e "s/\$\$MSC_TAG_PREFIX/$MSC_TAG_PREFIX_FOR_REPLACEMENT/" \
     -e "s/\$\$MSC_TAG_VERSION/$MSC_TAG_VERSION/" \
     -e "s/\$\$MSC_IMAGE_PULL_POLICY/$MSC_IMAGE_PULL_POLICY/" \
@@ -38,5 +39,13 @@ cat $MSC_KUBERNETES_FOLDER/reverse_proxy/reverse_proxy_minikube.yaml | sed \
     kubectl delete -n $MSC_NAMESPACE -f -
 
 kubectl delete serviceaccount msc-experimenter-service-account -n $MSC_NAMESPACE
+
+cat $MSC_KUBERNETES_FOLDER/analyzer/analyzer.yaml | sed \
+    -e "s/\$\$MSC_NAMESPACE/mastership/" \
+    -e "s/\$\$MSC_TAG_PREFIX/$MSC_TAG_PREFIX_FOR_REPLACEMENT/" \
+    -e "s/\$\$MSC_TAG_VERSION/$MSC_TAG_VERSION/" \
+    -e "s/\$\$MSC_IMAGE_PULL_POLICY/$MSC_IMAGE_PULL_POLICY/" \
+    -e "s/\$\$MSC_ANALYZER_SECRET_KEY/$MSC_ANALYZER_SECRET_KEY/" | \
+    kubectl delete -n default -f -
 
 kubectl delete namespace $MSC_NAMESPACE
